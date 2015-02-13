997e480f4caca215836684ae28748925977b8803
Eduard Carreras <ecarreras@gisce.net>
2015-02-13 17:04:06 +0100

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='raven-sanitize-openerp',
-    version='0.1.1',
+    version='0.1.2',
     author='GISCE-TI, SL',
     author_email='devel@gisce.net',
     url='http://code.gisce.net/raven-sanitize-openerp',
